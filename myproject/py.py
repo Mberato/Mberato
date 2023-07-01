9bbecc86d488335a1caf69a203030493a4f14125
Mberato <mohammad.bahrami138025@gmail.com>
2023-07-01 05:11:20 -0700

    hehehe you cane get Perimeter and area of ​​a rectangle

---
+++ myproject/py.py
@@ -0,0 +1,10 @@
+print('please type e or a for get Environment or Area')
+t=input()
+print('give me your width and length')
+x=int(input())
+y=int(input())
+if  t=='a':
+	print(x*y)
+elif t=='e':
+	ptint(2*(y+x))
+
